46d42e60fdd8a30cc866ffbb1760c964f3ff80d6
jaign <jaign@outlook.com>
2022-09-14 04:26:26 +0000

    开启pylint设置

--- app/tools/golang.py
+++ app/tools/golang.py
@@ -6,7 +6,7 @@ from lxml import etree
 from app.tools import consts
 
 
-def get_latest_version():
+def get_latest_version() -> tuple[int, int, int]:
     golang_html = consts.cache_dir.joinpath("golang.html")
     if golang_html.exists():
         with open(golang_html, "r", encoding="utf-8") as fr:
@@ -26,7 +26,7 @@ def get_latest_version():
     return max(versions)
 
 
-def get_all_versions():
+def get_all_versions() -> list[str]:
     golang_html = consts.cache_dir.joinpath("golang.html")
     if golang_html.exists():
         with open(golang_html, "r", encoding="utf-8") as fr:
@@ -37,6 +37,6 @@ def get_all_versions():
             html_txt = r.text
             fw.write(html_txt)
     html = etree.HTML(html_txt)
-    v_list = html.xpath('//div[@class="expanded"]/h2/text()')
-    versions = [v.strip().removeprefix("go").removesuffix(" ▾") for v in v_list]
+    v_list: list[str] = html.xpath('//div[@class="expanded"]/h2/text()')
+    versions: list[str] = [v.strip().removeprefix("go").removesuffix(" ▾") for v in v_list]
     return versions
